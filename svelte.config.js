dd0c330a0b559ac9703933b1b30a0c04bcbfee2b
Stefanos-gif <stefanossiathas@gmail.com>
2025-05-23 02:15:37 +0300

    fix: build error

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
-// svelte.config.js
 import adapter from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   preprocess: preprocess(),
   kit: {
     adapter: adapter({
-      // force Vercel to use Node.js 20.x for all functions
-      runtime: 'nodejs20.x'
+      runtime: 'nodejs18.x'
     })
   }
 };
-
-export default config;
